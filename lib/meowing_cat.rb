7d448aa853dd20dd9cdad54e593cbdb5e655cc6e
Jessica Watts <jessperr182@hotmail.com>
2020-09-15 19:31:01 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,8 +2,21 @@
 class Cat 
   attr_accessor :name 
   
-  def meow
-    puts "meow!
+  def name
+    @name
   end
-  
+
+  def name=(name)
+      @name = name
+  end
+
+  def meow
+    @meow = puts "meow!"
+  end
+
+  def meow=(meow)
+    @meow = meow
+  end
+
+
 end
